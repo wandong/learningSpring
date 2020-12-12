42bc58386b10827873420559e452abc0f36f63b0
wandong1188 <wudiadong@gmail.com>
2020-12-12 17:13:44 +0800

    mvn clean install ok

--- springRetry/src/main/java/com/dong/demo/RetryApplication.java
+++ springRetry/src/main/java/com/dong/demo/RetryApplication.java
@@ -2,11 +2,16 @@ package com.dong.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class RetryApplication {
 
+
     public static void main(String[] args) {
+
+        System.out.println("hello world");
+
         SpringApplication.run(RetryApplication.class, args);
     }
 
