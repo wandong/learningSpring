d1412080ad0cf016d37b59e5da3badcbbc00f8d4
wandong1188 <wudiadong@gmail.com>
2021-05-12 06:44:46 +0800

    模拟并发问题

---
+++ concurent/src/main/java/com/example/concurent/ConcurentApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurentApplication.class, args);
+    }
+
+}
---
+++ concurent/src/test/java/com/example/concurent/ConcurentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
